59394634ec7cae144f6dc23f02550273a6e01048
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-13 18:56:45 -0500

    Iteración 1 ciclo 3 (Rojo)

--- Test_procesador.py
+++ Test_procesador.py
@@ -9,3 +9,5 @@ class TestProcesador(TestCase):
     def test_contar_1 (self):
         self.assertEquals(Procesador().contador("1"),1,"Lista con un elemento")
 
+    def test_contar_2 (self):
+        self.assertEquals(Procesador().contador("1,2"),2,"Lista con dos elementos")
