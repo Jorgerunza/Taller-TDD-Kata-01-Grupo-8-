28a87da96918cea1d33ba957b9b048239dd13265
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-13 18:42:26 -0500

    Iteración 1 ciclo 2 (Rojo)

--- Test_procesador.py
+++ Test_procesador.py
@@ -2,5 +2,10 @@ from unittest import TestCase
 from Procesador import *
 
 class TestProcesador(TestCase):
+
     def test_contador(self):
         self.assertEquals(Procesador().contador(""),0,"Lista Vacia")
+
+    def test_contar_1 (self):
+        self.assertEquals(Procesador().contador("1"),1,"Lista con un elemento")
+
