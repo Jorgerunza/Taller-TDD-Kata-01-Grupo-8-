302861f9bb787425d4882233937155403feab03b
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-13 20:16:10 -0500

    Iteración 4 ciclo 2 (Verde)

--- Procesador.py
+++ Procesador.py
@@ -6,5 +6,5 @@ class Procesador:
             else:
                 numeros = str.split( lista, ',')
                 numeros = [int(i) for i in numeros]
-                respuesta =[len(numeros), min(numeros), max(numeros)]
+                respuesta =[len(numeros), min(numeros), max(numeros), numeros[0]]
                 return respuesta
