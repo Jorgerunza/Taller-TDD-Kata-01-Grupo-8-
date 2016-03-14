7a394abb8788cd27c16bec797ce2876c302efbb5
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-13 19:32:10 -0500

    Iteración 2 ciclo 1 (Verde)

--- Procesador.py
+++ Procesador.py
@@ -2,7 +2,7 @@
 class Procesador:
         def contador (self,lista):
             if (lista == ''):
-                return {0}
+                return [0,0]
             else:
                 numeros = str.split( lista, ',')
                 respuesta ={len(numeros)}
